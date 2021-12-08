16f612518ba6c52f8b3173c74c633b0c8a9f3cc5
shiruizhi <shiruizhi@blued.com>
2021-12-08 17:11:20 +0800

    feat: update

---
+++ board.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pygame
+import sys
+
+
+class Board:
+    def __init__(self):
+        pygame.init()
+        self.size = 120, 120
+        self.grid = 3
+        self.margin = self.size[0] / 3
+        self.screen = pygame.display.set_mode(self.size)
+        self.cross = pygame.image.load("cross.png")
+        self.cross = pygame.transform.scale(self.cross, (20,20))
+        self.circle = pygame.image.load("circle.png")
+        self.circle = pygame.transform.scale(self.circle, (20,20))
+        self.board = np.zeros((self.grid, self.grid), dtype=np.int8)
+        self.board[1, 1] = 1
+        self.board[1, 2] = 2
+        self.board_list = []
+        print(self.board)
+
+    def draw(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.screen.fill("0x272822")
+            self.draw_board()
+            self.draw_pieces()
+            pygame.display.update()
+
+    def draw_board(self):
+        for i in range(self.grid):
+            for j in range(self.grid):
+                self.board_list.append(
+                    pygame.draw.rect(
+                        self.screen, "0xF8F8F2", (i * 30, j * 30, 30, 30), 0
+                    )
+                )
+        pygame.draw.line(self.screen, "0xE69F66", (0, 30), (90, 30))
+        pygame.draw.line(self.screen, "0xE69F66", (0, 60), (90, 60))
+        pygame.draw.line(self.screen, "0xE69F66", (30, 0), (30, 90))
+        pygame.draw.line(self.screen, "0xE69F66", (60, 0), (60, 90))
+
+    def draw_pieces(self):
+        for i in range(3):
+            for j in range(3):
+                if self.board[i, j] == 1:
+                    self.screen.blit(self.circle, self.board_list[i * 3 + j])
+                elif self.board[i, j] == 2:
+                    self.screen.blit(self.cross, self.board_list[i * 3 + j])
+
+
+if __name__ == "__main__":
+    board = Board()
+    board.draw()
---
+++ comment.py
@@ -0,0 +1,11 @@
+# Palette	Hex Code
+# Background	#272822 #272822
+# Foreground	#F8F8F2 #F8F8F2
+# Comment	#75715E #75715E
+# Red	#F92672 #F92672
+# Orange	#FD971F #FD971F
+# Light Orange	#E69F66 #E69F66
+# Yellow	#E6DB74 #E6DB74
+# Green	#A6E22E #A6E22E
+# Blue	#66D9EF #66D9EF
+# Purple	#AE81FF #AE81FF
